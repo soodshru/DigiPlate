dfff082f31f843c175ed4bf05e5ff3a8a76310b8
Shruti Sood <shruti.sood@mail.utoronto.ca>
2021-09-18 01:01:23 -0400

    set mode to landscape

--- digiplate/lib/main.dart
+++ digiplate/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft])
+      .then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
