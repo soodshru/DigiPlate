ca67359c17815c25d96ee586d6b4df5cf1babc58
Shruti Sood <shruti.sood@mail.utoronto.ca>
2021-09-18 08:11:14 -0400

    removed some containers

--- digiplate/lib/screens/location_detail/location_detail.dart
+++ digiplate/lib/screens/location_detail/location_detail.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'image_banner.dart';
-import 'text_section.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextSection(Colors.red),
-            TextSection(Colors.green),
-          ]),
-    );
+        backgroundColor: const Color(0xffFFFFFF),
+        body: Center(
+            child: Text(
+          'HTN 2021',
+          style: TextStyle(
+              fontSize: 150, fontWeight: FontWeight.bold, color: Colors.black),
+        )));
   }
 }
