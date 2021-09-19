7a70ca0baf31d1dcdf041cf1bff90518da90c8c6
Abhishek Chatterjee <abhi1506@live.com>
2021-09-19 05:30:27 -0400

    changed code to simulate accident workflow

--- database/main.go
+++ database/main.go
@@ -12,13 +12,22 @@ import (
 )
 
 func insertRows(ctx context.Context, tx pgx.Tx, accts [3]uuid.UUID) error {
-	// Insert three rows into the "info" table.
-	log.Println("Creating new rows...")
+	// Initialising database
 	if _, err := tx.Exec(ctx,
-		"INSERT INTO info (id, driverPlate1, insuranceNo1, driverPlate2, insuranceNo2, locationX, locationY) VALUES ($1, $2, $3, $4, $5, $6, $7), ($8, $9, $10, $11, $12, $13, $14), ($15, $16, $17, $18, $19, $20, $21)", accts[0], "HTN 2021", 250, "UTM 1969", 300, -89, -50, accts[1], "HTV 1234", 100, "UOT 1123", 400, 112, -69, accts[2], "UTH 7890", 150, "UWO 8888", 200, 90, 180); err != nil {
+		"INSERT INTO info (id, driverPlate1, insuranceNo1, driverPlate2, insuranceNo2, locationX, locationY) VALUES ($1, $2, $3, $4, $5, $6, $7), ($8, $9, $10, $11, $12, $13, $14), ($15, $16, $17, $18, $19, $20, $21)", accts[0], "HTN-2021", 250, "UTM-1969", 300, -89, -50, accts[1], "HTV-1234", 100, "UOT-1123", 400, 112, -69, accts[2], "UTH-7890", 150, "UWO-8888", 200, 90, 180); err != nil {
 		return err
 	}
 	return nil
+}
+
+func insertRows2(ctx context.Context, tx pgx.Tx, accts [1]uuid.UUID) error {
+        // Insert row into the "info" table.
+        log.Println("Adding new row...")
+        if _, err := tx.Exec(ctx,
+                "INSERT INTO info (id, driverPlate1, insuranceNo1, driverPlate2, insuranceNo2, locationX, locationY) VALUES ($1, $2, $3, $4, $5, $6, $7)", accts[0], "ABC-9999", 101, "DEF-2222", 450, 121, -42); err != nil {
+                return err
+        }
+        return nil
 }
 
 func printBalances(conn *pgx.Conn) error {
@@ -39,16 +48,6 @@ func printBalances(conn *pgx.Conn) error {
 			log.Fatal(err)
 		}
 		log.Printf("%s: %s %d %s %d %d %d\n", id, plate1, insuranceNo1, plate2, insuranceNo2, locationX, locationY)
-	}
-	return nil
-}
-
-func deleteRows(ctx context.Context, tx pgx.Tx, one uuid.UUID, two uuid.UUID) error {
-	// Delete two rows into the "info" table.
-	log.Printf("Deleting rows with IDs %s and %s...", one, two)
-	if _, err := tx.Exec(ctx,
-		"DELETE FROM info WHERE id IN ($1, $2)", one, two); err != nil {
-		return err
 	}
 	return nil
 }
@@ -81,27 +80,27 @@ func main() {
 	err = crdbpgx.ExecuteTx(context.Background(), conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
 		return insertRows(context.Background(), tx, accounts)
 	})
-	if err == nil {
-		log.Println("New rows created.")
-	} else {
+	if err != nil {
 		log.Fatal("error: ", err)
 	}
 
 	// Print out the balances
-	log.Println("Initial balances:")
+	log.Println("Initial list:")
 	printBalances(conn)
 
-	// Delete rows
+	var accounts2 [1]uuid.UUID
+	accounts2[0] = uuid.New()
+
 	err = crdbpgx.ExecuteTx(context.Background(), conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
-		return deleteRows(context.Background(), tx, accounts[0], accounts[1])
-	})
+                return insertRows2(context.Background(), tx, accounts2)
+        })
 	if err == nil {
-		log.Println("Rows deleted.")
-	} else {
-		log.Fatal("error: ", err)
-	}
+                log.Println("New rows created.")
+        } else {
+                log.Fatal("error: ", err)
+        }
 
 	// Print out the balances
-	log.Println("Balances after deletion:")
+	log.Println("Final list:")
 	printBalances(conn)
 }
